627f53bc809dd9c1e0b5ddb9ceb645915b5e292a
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2022-02-17 00:51:49 -0300

    - First and second class about vue js of Alura; - Creation of project using vue CLI; - interpolation, vue bindings, v-for, and concepts of vue bootstrap.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: [
+        {
+          url:
+            "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/social/monkey_king.jpg",
+          titulo: "dota2-mk"
+        },
+        {
+          url:
+            "https://lucrorpg.com/wp-content/uploads/2021/08/king-louis-II-the-younger-of-italy.webp",
+          titulo: "ck3"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
