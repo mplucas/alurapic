cb631fe3c629eb976d349821854b25cb4228f494
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2022-02-21 23:59:18 -0300

    - routing with VueRouter; - component home; - component menu.

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,42 @@
 <template>
   <div class="corpo">
-    <h1 class="titulo">{{ titulo }}</h1>
+    <meu-menu :rotas="routes"></meu-menu>
 
-    <input
-      type="search"
-      class="filtro"
-      @input="filtro = $event.target.value"
-      placeholder="filtre pelo título da foto"
-    />
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel.vue";
-import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva.vue";
+import { routes } from "./routes.js";
+import Menu from "./components/shared/menu/Menu.vue";
 
 export default {
   components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    "meu-menu": Menu
   },
-
   data() {
     return {
-      titulo: "Alurapic",
-      fotos: [],
-      filtro: ""
+      routes
     };
-  },
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => (this.fotos = fotos));
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
   }
 };
 </script>
 
 <style>
-.titulo {
-  text-align: center;
-}
-
 .corpo {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
 }
 
-.lista-fotos {
-  list-style: none;
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
 }
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-/* estilo do painel */
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
 </style>
